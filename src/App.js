66865d37b1dc3203d1168e1aa9c03bf83b731908
val-fcn <valentin.faucon@gmail.com>
2020-06-26 16:25:40 +0200

    pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
     <div className="App">
       <p>Hello World !</p>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/leaderboard" component={Leaderboard} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>TITRE 2</h1>
+    </div>
+  );
+}
